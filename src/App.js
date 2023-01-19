9b8f8f74370488f69131b449790697578774279e
Alan Timbó <alancorreiatimbo@gmail.com>
2023-01-19 01:59:52 -0300

    initial settings

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./assets/styles/GlobalStyle";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+    </BrowserRouter>
+  );
+}
